8f86026cc685df00761c4899ac828bbe8aa97a6b
Stéphane <stepxan@yahoo.es>
2022-03-12 17:16:29 +0100

    creación modulo lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {
@@ -10,19 +12,33 @@ const routes: Routes = [
     component: HomeComponent
   },{
     path: 'about',
-    component: HomeComponent
+    component: AboutComponent
   },{
     path: 'contact',
-    component: HomeComponent
+    component: ContactComponent
   },
+  {
+  //  Ejemplo de carga perezoza o lazy load antigua y nueva
+    path: 'posts',
+    // loadChildren: './pages/posts/posts.module#PostsModule'
+    loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ]
 
 
 
 @NgModule({
-  declarations: [],
+  
   imports: [
-    CommonModule
+    // CommonModule: es el utizado para especificar a Angular como utilizar la directiva NgFor, NgIf entre otras cosas
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
